db091def62e5fc4746b46307b760ade7ad140663
Ahmed <aabdelmonem863@gmail.com>
2026-07-11 02:36:31 +0300

    test deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,34 +5,36 @@ import MenuSection from "./components/menu-section/menu-section";
 import ContactMap from "./components/contacts/contact-map";
 import HomeContact from "./components/contacts/home-contact-section";
 import Gallery from "./components/gallery/gallery";
-import { headers } from "next/headers";
+// import { headers } from "next/headers";
 
-async function getMenu() {
-  try {
-    const host = (await headers()).get("host");
-    const protocol =
-      process.env.NODE_ENV === "development" ? "http" : "https";
-console.log(process.env.NEXT_PUBLIC_BASE_URL);
-    const res = await fetch(`${protocol}://${host}/api/menu`, {
-      cache: "no-store",
-    });
+// async function getMenu() {
+//   try {
+//     const host = (await headers()).get("host");
+//     const protocol =
+//       process.env.NODE_ENV === "development" ? "http" : "https";
+// console.log(process.env.NEXT_PUBLIC_BASE_URL);
+//     const res = await fetch(`${protocol}://${host}/api/menu`, {
+//       cache: "no-store",
+//     });
 
-    if (!res.ok) {
-      throw new Error("Failed to fetch menu");
-    }
+//     if (!res.ok) {
+//       throw new Error("Failed to fetch menu");
+//     }
 
-    return await res.json();
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
-}
+//     return await res.json();
+//   } catch (error) {
+//     console.error(error);
+//     return [];
+//   }
+// }
 
 export default async function Home() {
-  const menu = await getMenu();
+  // const menu = await getMenu();
 
   return (
     <>
+          <h1 className="text-5xl text-red-500">TEST DEPLOY</h1>
+
       <HeroSection />
       <main>
         <SpecialDishes />
