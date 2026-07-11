2bced7d53493cfa5f33eeed8c3fb7cf4f6e290e6
Ahmed <aabdelmonem863@gmail.com>
2026-07-11 03:00:21 +0300

    last changes calling api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,44 +4,26 @@ import SpecialDishes from "./components/special-dishes/special-dishes";
 import MenuSection from "./components/menu-section/menu-section";
 import HomeContact from "./components/contacts/home-contact-section";
 import Gallery from "./components/gallery/gallery";
-import { headers } from "next/headers";
 
-async function getMenu() {
-  try {
-    const host = (await headers()).get("host");
-    const protocol =
-      process.env.NODE_ENV === "development" ? "http" : "https";
-console.log(process.env.NEXT_PUBLIC_BASE_URL);
-    const res = await fetch(`${protocol}://${host}/api/menu`, {
-      cache: "no-store",
-    });
+import menu from "@/src/data/menu.json";
 
-    if (!res.ok) {
-      throw new Error("Failed to fetch menu");
-    }
-
-    return await res.json();
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
-}
-
-export default async function Home() {
-  const menu = await getMenu();
-
+export default function Home() {
   return (
     <>
       <HeroSection />
+
       <main>
         <SpecialDishes />
+
         <Chefs />
+
         <MenuSection menu={menu} />
+
         <Gallery />
+
         <section>
           <HomeContact />
         </section>
-        
       </main>
     </>
   );
